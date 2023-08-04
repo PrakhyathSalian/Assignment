dd1aba16f9c46f36710094a26d707ae03c031864
PrakhyathSalian <138657200+PrakhyathSalian@users.noreply.github.com>
2023-08-04 17:07:28 +0530

    Add files via upload

---
+++ camel.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    word_count=0
+    for letter in s:
+        if letter.isupper():
+            word_count=word_count+1
+    word_count=word_count+1
+    return word_count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
